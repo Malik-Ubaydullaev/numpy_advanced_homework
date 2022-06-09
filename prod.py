4057a64224de1d488f3705f7cea888645ca86dce
Your Name <ubaydullayev@samtuit.uz>
2022-06-09 14:50:55 +0500

    prod

--- prod.py
+++ prod.py
@@ -8,3 +8,18 @@ def arr_product(arr: np.ndarray) -> int:
     Returns:
         int: product of all numbers
     """
+    Prod = 1
+    row, col = arr.shape
+    
+    for i in range(row):
+        for j in range(col):
+            Prod = Prod *  arr[i,j]
+    return Prod
+
+#arr = np.array([[ 1,  2,  3],
+#       [ 4,  5,  6],
+#       [ 7,  8,  9],
+#       [10, 11, 1]])
+#print(arr)
+#Proda = arr_product(arr)
+#print(Proda)
