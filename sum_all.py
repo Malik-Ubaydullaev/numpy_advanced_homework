472330e62b11cbbfe0104b35826e3c69012bf955
Your Name <ubaydullayev@samtuit.uz>
2022-06-09 14:38:07 +0500

    sum_all

--- sum_all.py
+++ sum_all.py
@@ -8,3 +8,19 @@ def sum_all(arr: np.ndarray) -> int:
     Returns:
         int: sum of all numbers
     """
+    Sum = 0
+    row, col = arr.shape
+    
+    
+    for i in range(row):
+        for j in range(col):
+            Sum = Sum +  arr[i,j]
+    return Sum
+
+#arr = np.array([[ 1,  2,  3],
+#       [ 4,  5,  6],
+#       [ 7,  8,  9],
+#       [10, 11, 0]])
+#print(arr)
+#Summa = sum_all(arr)
+#print(Summa)
