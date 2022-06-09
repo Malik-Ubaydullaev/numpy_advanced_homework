a6d1d3c0b7c97036366b0934e99d501869f0c39c
Your Name <ubaydullayev@samtuit.uz>
2022-06-09 14:22:40 +0500

    add_min

--- min_all.py
+++ min_all.py
@@ -8,3 +8,17 @@ def min_all(arr: np.ndarray) -> int:
     Returns:
         int: min of all numbers
     """
+    row, col = arr.shape
+    
+    mini = arr[0,0]
+    for i in range(row):
+        for j in range(col):
+            if arr[i,j] < mini:
+                mini = arr[i,j]
+    
+    return mini
+
+#arr = np.arange(10)
+#print(arr)
+#minimumi = min_all(arr)
+#print(minimumi)
