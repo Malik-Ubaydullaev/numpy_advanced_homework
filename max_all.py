09dfa0bf383cfe8d2d542bf986bd1d56726e1617
Your Name <ubaydullayev@samtuit.uz>
2022-06-09 14:07:56 +0500

    add_max

--- max_all.py
+++ max_all.py
@@ -8,3 +8,17 @@ def max_all(arr: np.ndarray) -> int:
     Returns:
         int: max of all numbers
     """
+    maximum = arr[0]
+    for i in range(10):
+        
+        if arr[i] > arr[0]:
+            maximum = arr[i]
+    
+    return maximum
+    
+
+#arr = np.arange(10)
+#arr = np.reshape(arr, (10, 1))
+#print(arr)
+#maximumi = max_all(arr)
+#print(maximumi)
